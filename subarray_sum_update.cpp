e2e5a3ea894416d6a2e90601817ecf5fe8adbac7
Sugarcane16 <cynthia.lu.0116@gmail.com>
2025-06-21 09:43:54 -0700

    figured out we need a THIRD ARRAY :|
    debugged code 😛

--- subarray_sum_update.cpp
+++ subarray_sum_update.cpp
@@ -14,7 +14,15 @@ calculate the same thing (sum of subarray. AGAIN)
 #include <random>
 using namespace std;
 int n, a, b;
-int sum(int k, int* arr) {
+int sum(int k, int* tree) {
+    int s = 0;
+    while (k >= 1) {
+        s += tree[k];
+        k -= k&-k;
+    }
+    return s;
+}
+int calcRangeSum(int k, int* arr) {
     int s = 0;
     int end_index = k - (k&-k);
     for (int i = end_index + 1; i <= k; i++){
@@ -33,32 +41,38 @@ int main(){
     cin >> n;
     int* arr = new int[n + 1];
     int* tree = new int[n + 1];
+    int* sum_array = new int[n + 1];
     cout << "Here is your array: ";
     // Initialize and use the array
     random_device rd;
     mt19937 gen(rd()); // Create and seed the generator once
     uniform_int_distribution<int> distribution;
-    for (int i = 0; i < n + 1; ++i) {
+    for (int i = 1; i < n + 1; ++i) {
         int randomNumber = distribution(gen); // Generate random number
         arr[i] = randomNumber % 100;
         cout << arr[i] << " ";
     }
     cout << endl;
     for (int i = 1; i <= n; i++){
-        tree[i] = sum(i, arr);
+        tree[i] = calcRangeSum(i, arr);
     }
 
     cout << "The array tree is as follows:" << endl;
-    for (int i = 0; i <= n; i++){
+    for (int i = 1; i <= n; i++){
         cout << tree[i] << " ";
     }
     cout << endl;
-    // yay done
-    cout << "Please enter a and b, the start and end indexes for the subarray, with a being less than b: ";
+    // now get da SUM >:(
+    cout << "The sum array is as follows:" << endl;
+    for (int i = 1; i <= n; i++){
+        sum_array[i] = sum(i, tree);
+        cout << sum_array[i] << " ";
+    }
+    cout << "Please enter a and b, the start and end indexes for the subarray, with a being less than b: " << endl;
     cin >> a >> b;
     cout << "a and b are, respectively, " << a << " and " << b << endl;
     if (a <= n && b <= n && a > 0 && b >= 0 && a < b){
-        int a_b_sum = tree[b] - tree[a - 1];
+        int a_b_sum = sum_array[b] - sum_array[a - 1];
         cout << "The sum of all the values between index a and b, inclusive, is " << a_b_sum << endl;
     } else{
         cout << "Invalid input detected: either a and/or b are out of bounds for the array index values, or a is not less than b, or both. pls try again. :(";
@@ -67,9 +81,18 @@ int main(){
     int c, d;
     cin >> c >> d;
     if (c >= a && c <= b){
-        int x = d - tree[c];
+        int x = d - arr[c];
         add(c, x, tree);
-        int new_a_b_sum = tree[b] - tree[a - 1];
+        for (int i = 1; i <= n; i++){
+            sum_array[i] = sum(i, tree);
+        }
+        cout << "This is x : " << x << endl;
+        cout << "Now the new array is: " << endl;
+        for (int i = 1; i <= n; i++){
+            cout << sum_array[i] << " ";
+        }
+        cout << endl;
+        int new_a_b_sum = sum_array[b] - sum_array[a - 1];
         cout << "The updated sum of all the values between index a and b, inclusive, is " << new_a_b_sum << endl;
     } else{
         cout << "Invalid input detected: c is not an index between indices a and b, inclusive. pls try again. :(";
